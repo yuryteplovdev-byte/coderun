5b17e18669ce020afcb9423ace16e9e8f41629fd
Yuriy Teplov <yury.teplov.dev@gmail.com>
2026-01-26 10:18:35 +0500

    57 success completed

--- 57.py
+++ 57.py
@@ -1,12 +1,21 @@
 import sys
 
 def main():
-    """
-    Пример ввода и вывода числа n, где -10^9 < n < 10^9:
-    n = int(input())
-    print(n)
-    """
-    pass
+    first_gen = input()
+    second_gen = input()
+
+    pair_counter = 0
+
+    second_gen_pairs = {}
+
+    for i in range(len(second_gen)-1):
+        second_gen_pairs[second_gen[i] + second_gen[i+1]] = True
+
+    for i in range(len(first_gen)-1):
+        if second_gen_pairs.get(first_gen[i] + first_gen[i+1]) == True:
+            pair_counter += 1
+
+    print(pair_counter)
 
 if __name__ == '__main__':
     main()
